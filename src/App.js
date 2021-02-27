45ce71e190dbaefdfa9941e6787ef1ea72a4471f
sCerdan <santyjoell@gmail.com>
2021-02-27 15:53:03 -0300

    Se arma el esqueleto de la app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  margin: 0;
+  max-width: 100vw;
+  display: flex;
+  background-color: #290149;
+  justify-content: center;
+  align-items: center;
+  height: 5vh;
+  color: white;
+`;
+
+const ContenedorMain = styled.main`
+  margin: 0;
+  max-width: 100vw;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 75vh;
+  color: white;
+`;
+
+const MainBox = styled.div`
+  width: 35rem;
+  display: flex;
+  background-color: #290149;
+  height: 35rem;
+  color: white;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Contenedor> 
+      <h1>Cripto App</h1>
+    </Contenedor>
+    <ContenedorMain>
+        <MainBox>1</MainBox>
+        <MainBox>2</MainBox>
+        <MainBox>3</MainBox>
+      </ContenedorMain>
+    </>
   );
 }
 
